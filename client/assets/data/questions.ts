bfa2dd9c7ba055503ea54216a943428f8493bd36
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-28 22:25:19 +0800

    Add: questions mock data

---
+++ client/assets/data/questions.ts
@@ -0,0 +1,52 @@
+export const questions = [
+  {
+    _id: "1",
+    category: "oop",
+    question: "What is an example of dynamic binding?",
+    choices: [
+      { id: "a", text: "any method" },
+      { id: "b", text: "method overloading" },
+      { id: "c", text: "method overriding" },
+      { id: "d", text: "compiling" },
+    ],
+    answer: "c",
+  },
+  {
+    _id: "2",
+    category: "oop",
+    question:
+      "For which case would the use of a static attribute be appropriate?",
+    choices: [
+      {
+        id: "a",
+        text: "the number of people in each house in a small neighborhood",
+      },
+      { id: "b", text: "the lot size for each house in a small neighborhood" },
+      { id: "c", text: "the color of each house in a small neighborhood" },
+      {
+        id: "d",
+        text: "the weather conditions for each house in a small neighborhood",
+      },
+    ],
+    answer: "d",
+  },
+  {
+    _id: "3",
+    category: "oop",
+    question:
+      "Why would you create an abstract class, if it can have no real instances?",
+    choices: [
+      {
+        id: "a",
+        text: "to avoid redundant coding in children",
+      },
+      { id: "b", text: "to explore a hypothetical class" },
+      { id: "c", text: "to prevent unwanted method implementation" },
+      {
+        id: "d",
+        text: "to reserve memory for an unspecified class type",
+      },
+    ],
+    answer: "a",
+  },
+];
