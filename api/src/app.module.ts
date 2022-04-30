2ea04a92a63e379c833eceb77ef2d4750e719f27
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-30 21:42:18 +0800

    add: modules

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { QuestionsModule } from './questions/questions.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DB_URI),
+    QuestionsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
