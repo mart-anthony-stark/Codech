6f9a88f3096bc0b7ae358df66352764f25139e0f
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-02 00:07:43 +0800

    Add: user model

---
+++ api/src/auth/models/user.model.ts
@@ -0,0 +1,8 @@
+import * as mongoose from 'mongoose';
+
+export const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['basic', 'admin'], default: 'basic' },
+});
