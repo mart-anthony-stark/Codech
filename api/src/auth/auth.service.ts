b0f0dbb43aa1a8af3abe13238aa2cb3316fd6fe8
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-02 00:06:56 +0800

    Add: auth service

---
+++ api/src/auth/auth.service.ts
@@ -0,0 +1,60 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { IUser } from './interface';
+import { LoginDto, SignupDto } from './dto';
+import * as argon from 'argon2';
+import { JwtService } from '@nestjs/jwt';
+import { NotFoundError } from 'rxjs';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectModel('User') private readonly userModel: Model<IUser>,
+    private jwt: JwtService,
+  ) {}
+
+  async signup(dto: SignupDto) {
+    const exists = await this.userModel.findOne({ email: dto.email });
+
+    if (exists)
+      throw new HttpException(
+        'Email is already registered to an account',
+        HttpStatus.CONFLICT,
+      );
+
+    const user = new this.userModel(dto);
+    const hashed = await argon.hash(dto.password);
+    user.password = hashed;
+
+    await user.save();
+
+    return this.signToken(user._id, user.email);
+  }
+
+  async login(dto: LoginDto) {
+    const { email, password } = dto;
+    const user = await this.userModel.findOne({ email });
+    if (!user)
+      throw new HttpException('User account not found', HttpStatus.NOT_FOUND);
+
+    const match = await argon.verify(user.password, password);
+    if (!match)
+      throw new HttpException('Incorrect password', HttpStatus.UNAUTHORIZED);
+
+    return this.signToken(user._id, user.email);
+  }
+
+  signToken(userId, email: string): { access_token: string } {
+    const payload = {
+      sub: userId,
+      email: email,
+    };
+    const token = this.jwt.sign(payload, {
+      expiresIn: '7d',
+      secret: process.env.JWT_SECRET,
+    });
+
+    return { access_token: token };
+  }
+}
