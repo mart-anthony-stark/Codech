77a9f8073c27bd9897c6959a2f2ddc59234a7573
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-02 00:06:40 +0800

    Add: auth module

---
+++ api/src/auth/auth.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UserSchema } from './models';
+
+@Module({
+  imports: [
+    JwtModule.register({}),
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
