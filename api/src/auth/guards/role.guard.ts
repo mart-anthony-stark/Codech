609a39177d64532f8ff8dcff881e1e93d7422171
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-04 00:05:13 +0800

    Add: role guard

---
+++ api/src/auth/guards/role.guard.ts
@@ -0,0 +1,33 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const requiredRole = this.reflector.getAllAndOverride('role', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    console.log(requiredRole);
+    console.log(request.user);
+    if (request.user.role !== requiredRole)
+      throw new HttpException(
+        `You must be an ${requiredRole} to access`,
+        HttpStatus.FORBIDDEN,
+      );
+
+    return true;
+  }
+}
