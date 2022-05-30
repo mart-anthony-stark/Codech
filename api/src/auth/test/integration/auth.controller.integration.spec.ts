f2d55e2b7f8a9ed90c1a74f28ac93e13c3b7102c
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-30 20:30:31 +0800

    Add: db connection for testing

--- api/src/auth/test/integration/auth.controller.integration.spec.ts
+++ api/src/auth/test/integration/auth.controller.integration.spec.ts
@@ -1,12 +1,25 @@
 import { Test } from '@nestjs/testing';
 import { AppModule } from 'src/app.module';
+import { Connection } from 'mongoose';
+import { DatabaseService } from '../../../database/database.service';
+import { userStub } from '../stubs/user.stub';
 
 describe('AuthController', () => {
+  let dbConnection: Connection;
+
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
     const app = moduleRef.createNestApplication();
     await app.init();
+
+    dbConnection = moduleRef
+      .get<DatabaseService>(DatabaseService)
+      .getDbHandle();
+  });
+
+  describe('getUsers', async () => {
+    await dbConnection.collection('users').insertOne(userStub());
   });
 });
