a851106b6b8c26451a904d78b28b8ca743abef79
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-30 21:53:39 +0800

    Fixed: default role

--- api/src/auth/models/user.model.ts
+++ api/src/auth/models/user.model.ts
@@ -4,5 +4,5 @@ export const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['user', 'admin'], default: 'basic' },
+  role: { type: String, enum: ['user', 'admin'], default: 'user' },
 });
