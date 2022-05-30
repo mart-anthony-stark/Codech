713c3f204a758c75414456ccdc093e28356e088d
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-30 20:39:25 +0800

    moved instantiation of db to db module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,18 +1,15 @@
 import { Module } from '@nestjs/common';
 import { QuestionsModule } from './questions/questions.module';
 import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
-import { JwtModule, JwtService } from '@nestjs/jwt';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.DB_URI),
     QuestionsModule,
     AuthModule,
-    DatabaseModule
+    DatabaseModule,
   ],
   controllers: [],
   providers: [],
