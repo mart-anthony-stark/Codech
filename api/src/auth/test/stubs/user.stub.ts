0fb103b9acbb15f86cd02b5b7999b4dc6e0f5d41
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-30 21:54:02 +0800

    Add: user stub

--- api/src/auth/test/stubs/user.stub.ts
+++ api/src/auth/test/stubs/user.stub.ts
@@ -3,7 +3,7 @@ import { IUser } from 'src/auth/interface';
 export const userStub = (): IUser => {
   return {
     email: 'mart@gmail.com',
-    name: 'mart',
+    name: 'Mart Anthony',
     password: 'password',
     role: 'user',
   };
