66cc29b7c01ee2855a1bfa4ae3374a8b0ed79e3b
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-30 20:39:44 +0800

    Add: db instantiation to mongoose module

--- api/src/database/database.module.ts
+++ api/src/database/database.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { DatabaseService } from './database.service';
 
 @Module({
+  imports: [MongooseModule.forRoot(process.env.DB_URI)],
   providers: [DatabaseService],
   exports: [DatabaseService],
 })
