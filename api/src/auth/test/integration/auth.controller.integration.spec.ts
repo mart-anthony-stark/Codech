b99d5a91481c8ec2a64a08ed5039e8a3d138e1ec
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-30 21:53:55 +0800

    Add: e2e testing for auth

--- api/src/auth/test/integration/auth.controller.integration.spec.ts
+++ api/src/auth/test/integration/auth.controller.integration.spec.ts
@@ -1,25 +1,65 @@
 import { Test } from '@nestjs/testing';
-import { AppModule } from 'src/app.module';
+import { AppModule } from '../../../app.module';
 import { Connection } from 'mongoose';
 import { DatabaseService } from '../../../database/database.service';
 import { userStub } from '../stubs/user.stub';
+import * as request from 'supertest';
 
 describe('AuthController', () => {
   let dbConnection: Connection;
+  let httpServer: any;
+  let app: any;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
-    const app = moduleRef.createNestApplication();
+    app = moduleRef.createNestApplication();
     await app.init();
 
     dbConnection = moduleRef
       .get<DatabaseService>(DatabaseService)
       .getDbHandle();
+    httpServer = app.getHttpServer();
   });
 
-  describe('getUsers', async () => {
-    await dbConnection.collection('users').insertOne(userStub());
+  afterAll(async () => {
+    await app.close();
+  });
+
+  beforeEach(async () => {
+    await dbConnection.collection('users').deleteMany({});
+  });
+
+  describe('Get Users', () => {
+    it('Should return an array of users', async () => {
+      await dbConnection.collection('users').insertOne(userStub());
+      const response = await request(httpServer).get('/auth/users');
+
+      expect(response.status).toBe(200);
+      expect(response.body).toMatchObject([userStub()]);
+    });
+  });
+
+  describe('Register User', () => {
+    it('Should create new user', async () => {
+      const createUserRequest = {
+        name: userStub().name,
+        email: userStub().email,
+        password: userStub().password,
+      };
+      const response = await request(httpServer)
+        .post('/auth/signup')
+        .send(createUserRequest);
+
+      expect(response.status).toBe(201);
+      expect(response.body).toMatchObject(createUserRequest);
+
+      const user = await dbConnection
+        .collection('users')
+        .findOne({ email: createUserRequest.email });
+
+      expect(user).toMatchObject(createUserRequest);
+    });
   });
 });
