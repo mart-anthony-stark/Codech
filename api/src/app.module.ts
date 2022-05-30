9defb21d29e6984e490d1bdb5583c4e6940272bb
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-30 20:30:19 +0800

    Add: database module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { JwtModule, JwtService } from '@nestjs/jwt';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { JwtModule, JwtService } from '@nestjs/jwt';
     MongooseModule.forRoot(process.env.DB_URI),
     QuestionsModule,
     AuthModule,
+    DatabaseModule
   ],
   controllers: [],
   providers: [],
